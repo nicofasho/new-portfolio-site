9e44aa60eb6a6de87c8b15aa31a309f94ebfb4a7
Christian Watson <chris23w@gmail.com>
2019-09-09 14:16:03 -0700

    homepage converted to react

--- src/App.js
+++ src/App.js
@@ -1,90 +1,219 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="wrapper">
-    <header>
-        <nav className="header">
-            <a className="logo" href="index.html">Christian Watson</a>
-            <ul className="navigation">
-                <li>
-                    <a href="index.html">Home</a>
-                </li>
-                <li>
-                    <a href="about.html">About</a>
-                </li>
-                <li>
-                    <a href="contact.html">Contact</a>
-                </li>
+      <header>
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <a class="navbar-brand" id="top" href="/">
+            <img src="/images/Fleur-de-lis-fill.svg" alt="logo" class="logo" />
+          </a>
+
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="/">
+                  Portfolio <span class="sr-only">(current)</span>
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="/about.html">
+                  About
+                </a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="/contact.html">
+                  Contact
+                </a>
+              </li>
             </ul>
+          </div>
         </nav>
-    </header>
-    <h1>Hi, my name is Christian Watson, and I am a Software Engineer looking for employment in the Bay Area.</h1>
-    <h2>Welcome to my portfolio site!</h2>
+      </header>
 
-    <p className="hometext center">Here are some of my recent projects, enjoy!</p>
+      <div class="container">
+        <p class="header">Hi! I'm Christian Watson</p>
 
+        <p class="project-description">
+          I'm a Software Engineer currently living in the San Francisco Bay
+          Area.
+        </p>
 
-    <div className="columns-container">
-        <div className="column-4 project">
-            <div className="project-preview">
-                <img src="img/OnboardingPage.png" alt="Phlash" />
-                <h2>Phlash</h2>
-            </div>
-            <a href="phlash.html" className="project-link">
-                <p className="project-details">Phlash is a flash card app that served as a broad overview of the entire UX Design Process.</p>
-            </a>
-        </div>
-        <div className="column-4 project">
-            <div className="project-preview">
-                <img src="img/WelcomeScreenAndroid.png" alt="Schnitzeljagd" />
-                <h2>Schnitzeljagd</h2>
-            </div>
-            <a href="schnitzeljagd.html" className="project-link">
-                <p className="project-details">Schnitzeljagd is a scavenger hunt app that covers Competitive Research through to finished Presentation
-                    mockups and Dev Handoff.</p>
-            </a>
-        </div>
-        <div className="column-4 project">
-            <div className="project-preview">
-                <img src="img/MobileLogin.png" alt="Fitted" />
-                <h2>Fitted</h2>
-            </div>
-            <a href="fitted.html" className="project-link">
-                <p className="project-details">Fitted is a fitness responsive web app. This project focused on visual design for different breakpoints,
-                    UI Elements, and slicing elements for Developer Handoff.</p>
-            </a>
-        </div>
-        <div className="column-4 project">
-            <div className="project-preview">
-                <img src="img/tulanehomesnip.png" alt="Tulane Anesthesiology 2015" />
-                <h2>Tulane Anesthesiology 2015</h2>
-            </div>
-            <a href="tulane.html" className="project-link">
-                <p className="project-details">2015 website redesign for the Tulane University Department of Anesthesiology.</p>
-            </a>
-        </div>
-        
-    </div>
+        <div class="project">
+          <a href="/los.html">
+            <img
+              src="./images/LOSCoverImage.png"
+              alt="Little Orchestra Society Website Wireframes"
+              class="project-image img-fluid"
+            />
+          </a>
 
-    <footer>
-        <div className="footer">
-            <span>&copy; Christian Watson 2018</span>
-            <ul className="footernav">
-                <li>
-                    <a href="index.html">Home</a>
-                </li>
-                <li>
-                    <a href="about.html">About</a>
-                </li>
-                <li>
-                    <a href="contact.html">Contact</a>
-                </li>
+          <a href="/los.html">
+            <h2 class="project-title">
+              Little Orchestra Society Website Wireframes
+            </h2>
+          </a>
+
+          <p class="project-description">
+            This project was a set of wireframes for the Little Orchestra
+            Society's new website. LOS is a charitable organization in New York
+            City, NY.
+          </p>
+
+          <hr />
+        </div>
+
+        <div class="project">
+          <a href="/fitted.html">
+            <img
+              src="./images/fittedscreensmockup.jpg"
+              alt="Fitted"
+              class="project-image img-fluid"
+            />
+          </a>
+
+          <a href="/fitted.html">
+            <h2 class="project-title">Fitted</h2>
+          </a>
+
+          <p class="project-description">
+            Fitted is a responsive exercise app that motivates users to lead a
+            healthier lifestyle through easy to put together workout routines.
+          </p>
+
+          <hr />
+        </div>
+
+        <div class="project">
+          <a href="/schnitzeljagd.html">
+            <img
+              src="./images/SchnitzeljagdScreensMockup.jpg"
+              alt="Schnitzeljagd"
+              class="project-image img-fluid"
+            />
+          </a>
+
+          <a href="/schnitzeljagd.html">
+            <h2 class="project-title">Schnitzeljagd</h2>
+          </a>
+
+          <p class="project-description">
+            Schnitzeljagd is a scavenger hunt app that allows users to have fun
+            exploring their surroundings with friends and family.
+          </p>
+
+          <hr />
+        </div>
+
+        <div class="project">
+          <a href="/phlash.html">
+            <img
+              src="./images/PhlashMockup.jpg"
+              alt="Phlash"
+              class="project-image img-fluid"
+            />
+          </a>
+
+          <a href="/phlash.html">
+            <h2 class="project-title">Phlash</h2>
+          </a>
+
+          <p class="project-description">
+            Phlash is a flash card app concept that allows users to easily
+            create flash card decks for any subject that they want to learn.
+          </p>
+
+          <hr />
+        </div>
+
+        <div class="project">
+          <a href="/tuanes.html">
+            <img
+              src="./images/tuanesmockup.jpg"
+              alt="Tulane Anesthesiology 2015"
+              class="project-image img-fluid"
+            />
+          </a>
+
+          <a href="/tuanes.html">
+            <h2 class="project-title">Tulane Website Redesign 2015</h2>
+          </a>
+
+          <p class="project-description">
+            This project was a website for the Tulane University Department of
+            Anesthesiology completed in 2015. I solo designed all visual
+            elements: color, typography, and layout.
+          </p>
+        </div>
+
+        <hr class="mt-5" />
+
+        <footer class="d-flex justify-content-between">
+          <div class="mr-auto footer-section">
+            <ul class="footer-list">
+              <li>© Christian Watson 2019</li>
+
+              <li>All Rights Reserved</li>
             </ul>
-        </div>
-    </footer>
-</div>
+          </div>
+
+          <div class="footer-section">
+            <a href="#top">Page Top</a>
+          </div>
+
+          <div class="ml-auto footer-section">
+            <ul class="footer-list">
+              <li>
+                <a href="mailto:chris23w@gmail.com">Email</a>
+              </li>
+
+              <li>
+                <a href="https://www.linkedin.com/in/christian-watson-12875466/">
+                  LinkedIn
+                </a>
+              </li>
+
+              <li class="d-none">
+                <a href="https://twitter.com/WatsoAwesome">Twitter</a>
+              </li>
+            </ul>
+          </div>
+        </footer>
+      </div>
+
+      <script
+        src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"
+      ></script>
+
+      <script
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"
+      ></script>
+
+      <script
+        src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"
+      ></script>
+    </div>
   );
 }
 
